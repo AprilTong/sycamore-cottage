24e3a2e3823d5ec942c086db6ef073cff4dbfccc
april <april@zen-game.cn>
2023-10-07 17:43:41 +0800

    test: 测试打包

---
+++ assets/icon-27a63b47.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{o as c,c as e}from"./index-5c31601d.js";const n={},t={class:"iconfont icon-shezhi"};function s(r,_){return c(),e("i",t)}const f=o(n,[["render",s]]);export{f as default};
